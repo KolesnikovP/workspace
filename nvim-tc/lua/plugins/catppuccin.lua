d7ac2617bf4649017152ebaf98391b966b1bdf70
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-02-19 00:38:05 -0600

    transparency

--- nvim-tc/lua/plugins/catppuccin.lua
+++ nvim-tc/lua/plugins/catppuccin.lua
@@ -1,8 +1,12 @@
-return { 
+return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
+    local config = require("catppuccin")
+    config.setup({
+      transparent_background = true
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
