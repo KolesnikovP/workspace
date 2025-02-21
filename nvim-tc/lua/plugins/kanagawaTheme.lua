eeecf162c7bfbfcedb2f9e17fecc7c251418e770
Petr Kolesnikov <32188309+KolesnikovP@users.noreply.github.com>
2025-02-20 22:45:03 -0600

    relative number

--- nvim-tc/lua/plugins/kanagawaTheme.lua
+++ nvim-tc/lua/plugins/kanagawaTheme.lua
@@ -5,13 +5,21 @@ return {
     local config = require("kanagawa")
     config.setup({
       transparent = true,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
+--      theme = "wave", -- Load "wave" theme when 'background' option is not set
       background = { -- map the value of 'background' option to a theme
-        -- 				dark = "wave", -- try "wave" !
+        				dark = "wave", -- try "wave" !
         --				light = "lotus",
       },
     })
     vim.cmd("colorscheme kanagawa")
+      -- Fix transparency issue
+    vim.cmd [[
+      highlight Normal guibg=NONE
+      highlight NormalNC guibg=NONE
+      highlight LineNr guibg=NONE
+      highlight CursorLineNr guibg=NONE
+    ]]
+
     -- vim.cmd("colorscheme kanagawa-dragon")
     --  vim.cmd("colorscheme kanagawa-wave")
   end,
