924dbaef6bf36c183511abb51270c2675aecafde
petr <ptrklsnkv@gmail.com>
2025-09-21 08:37:48 -0500

    change layot for telescop searching

--- nvim-tc/lua/plugins/telescope.lua
+++ nvim-tc/lua/plugins/telescope.lua
@@ -1,21 +1,33 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
   },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  },
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+          -- height = 0.95,
+          -- width = 0.9,
+         -- preview_height = 0.55,
+          prompt_position = "top",
+        },
+        preview_cutoff = 1,
+      },
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({
+            previewer = false,
+            prompt_title = false,
+          }),
+        },
+      },
+    })
+
+    telescope.load_extension("ui-select")
+  end,
 }
